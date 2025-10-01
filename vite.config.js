0bd3c7b9655f796881140aba82f61627a7bf9e00
angelinaash5456-a11y <angelinaash5456@gmail.com>
2025-10-01 19:28:04 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.skel'],
-  base: '/Spine-Viewer-Web/',
+  base: '/......./',
 })
